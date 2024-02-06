0df0f8e9bacdf8b55a84d8b0fa4ee3248c37fb45
JoseDavidEsquivel <122810644+JoseDavidEsquivel@users.noreply.github.com>
2024-02-06 12:39:29 -0600

    correcion de la app2

--- aplicacion_1/app.py
+++ aplicacion_1/app.py
@@ -1,13 +1,17 @@
 import web
 
-urls = ('/(.*)', 'hello')
+urls = ("/", "hello",
+        "/pagina2","pagina2"
+        )
 app = web.application(urls, globals())
 
 class hello:
-    def GET(self, name):
-        if not name:
-            name = 'World'
-        return 'Hola, ' + name + '!'
+    def GET(self):
+        return "Hello , pagina1"
+
+class pagina2:
+    def GET(self):
+        return "Hello , pagina2"
 
 if __name__ == "__main__":
     app.run()
