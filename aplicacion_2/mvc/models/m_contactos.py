867ebafb96df743f0e3dfd2ce647999c35679cb8
JoseDavidEsquivel <122810644+JoseDavidEsquivel@users.noreply.github.com>
2024-02-12 08:29:56 -0600

    Creacion de models

---
+++ aplicacion_2/mvc/models/m_contactos.py
@@ -0,0 +1,9 @@
+import web
+
+class ModeloContactos:
+    def __init__(self):
+        self.contactos = [
+    {'nombre': 'Juan', 'correo': 'juan@example.com', 'telefono': '123456789'},
+    {'nombre': 'María', 'correo': 'maria@example.com', 'telefono': '987654321'},
+    {'nombre': 'Pedro', 'correo': 'pedro@example.com', 'telefono': '456789123'}
+]
