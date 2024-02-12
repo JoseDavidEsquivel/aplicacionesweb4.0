e0d7e3bf847d50928d876b1bd49db336285b3bdc
JoseDavidEsquivel <122810644+JoseDavidEsquivel@users.noreply.github.com>
2024-02-12 08:25:31 -0600

    Creacion de models

--- aplicacion_2/mvc/controllers/index.py
+++ aplicacion_2/mvc/controllers/index.py
@@ -1,11 +1,19 @@
 import web
 
+from mvc.models.m_index import ModeloIndex
+
 render = web.template.render('mvc/views/')
+
+m_index = ModeloIndex()
    
 class Index:
     def GET(self):
         try:
-            return render.index()
+            datos = [
+                m_index.nombre,
+                m_index.email
+            ]
+            return render.index(datos)
         except Exception as e:
             print (f"Error 101 - index {e.args}")
             return render.error()
---
+++ aplicacion_2/mvc/models/m_index.py
@@ -0,0 +1,6 @@
+import web
+
+class ModeloIndex:
+    def __init__(self):
+        self.nombre="Dejah"
+        self.email="dejah@gmail.com"
